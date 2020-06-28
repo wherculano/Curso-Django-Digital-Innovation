362d6f622a3e5aa4ce50c223ed2d696021915efa
Wagner Herculano <wagherculano@hotmail.com>
2020-06-28 16:38:49 -0300

    Finalizando aula 2: criando tabelas de eventos

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    '''Campos que irão aparecer na pagina do django-admin'''
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('usuario', 'data_evento')
+
+admin.site.register(Evento, EventoAdmin) # registrando o modelo Evento que foi criado no models.py
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) # max 100 caracteres
+    descricao = models.TextField(blank=True, null=True) # Pode ser 'branco' e/ou 'nulo'
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True) # preenche o registro com a hora atual
+    # pegar a tabela de usuarios do Django para usa-los como usuarios da agenda.
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # no on_delete, ao deletar o usuario, seus eventos tbm serão
+
+
+    class Meta:
+        '''Exigindo que minha tabela se chame "evento" na criação do BD'''
+        db_table = 'evento'
+    
+    def __str__(self):
+        return self.titulo # no django-admin irá mostrar o titulo do envendo ao inves de Evento.object
